ae1353b722b39925eed0fab1dae9758e8d6196e4
RickyCurci <ric.cur@yahoo.com>
2020-08-20 17:16:42 +0200

    *

--- Python/PROJECT/Ideas/Exchange.py
+++ Python/PROJECT/Ideas/Exchange.py
@@ -2,6 +2,11 @@
 import requests
 import json
 
+first = 'EUR'
+second = 'USD'
+
+
+
 def Exchange(first, second):
 
     payload = {'base': first, 'symbols': second}
@@ -11,9 +16,12 @@ def Exchange(first, second):
     Json = response.text
     Python = json.loads(Json)
 
+    #Value Conversion
     RATES = Python
-
     Convert_Rate = RATES['rates'][second]
+
+    Quantity = 900
+    Convert_Rate = Convert_Rate * Quantity
     print(Convert_Rate)
 
-Exchange('EUR', 'JPY')
+Exchange(first, second)
