3e3ac3ae9d9bccf073b21a27d72cacbdba57ef84
RickyCurci <ric.cur@yahoo.com>
2020-08-22 20:37:00 +0200

    Update 2-ID.py

--- Python/PROJECT/Ideas/10/2-ID.py
+++ Python/PROJECT/Ideas/10/2-ID.py
@@ -1,61 +1,42 @@
 #!/usr/bin/python
-#NUMBER GUESSING
+#NUMBER GUESSING [ 2 ]
 
 from random import *
 
 min = randint(0, 50)
 MAX = randint(50, 100)
 
-Random_Number = 10
-
-
 def Guessing_Number_Game():
 
-    R = 0
-
     Random_Number = randint(min, MAX)
-    print(Random_Number)
 
     print('range = [ '+str(min)+' | '+str(MAX)+' ]')
     def Round():
 
         Guessing_Number = int(input('Number: '))
-
-        while R < 5:
-            if Guessing_Number == Random_Number:
-                print('YOU WIN THE GAME!! ')
-                print('CONGRATULATION')
-                print('IF YOU WANT PLAY AGAIN PRESS [ 1 ]')
-                print('OR TO EXIT PRESS [ 0 ]')
-                Status = int(input(': '))
-                if Status == 1:
-                    Guessing_Number_Game()
-                elif Status == 0:
-                    print('BYE')
-
-            elif Guessing_Number != Random_Number and R < 5:
-                print('THE NUMBER IS WRONG')
-                if Guessing_Number < Random_Number:
-                    print('YOUR NUMBER IS LESS THAN RANDOM NUMBER')
-                    R = R + 1
-                    print(R)
-                    Round()
-                elif Guessing_Number > Random_Number:
-                    print('YOUR NUMBER IS GRATER THAN RANDOM NUMBER')
-                    R = R + 1
-                    print(R)
-                    Round()
-
-
-        if R == 5:
-            print('GAME OVER!!')
+        if Guessing_Number == Random_Number:
+            print('YOU WIN THE GAME!! ')
+            print('CONGRATULATION')
             print('IF YOU WANT PLAY AGAIN PRESS [ 1 ]')
             print('OR TO EXIT PRESS [ 0 ]')
             Status = int(input(': '))
             if Status == 1:
                 Guessing_Number_Game()
-
             elif Status == 0:
-                ('BYE!!')
+                print('BYE')
+
+        elif Guessing_Number != Random_Number:
+            print('THE NUMBER IS WRONG')
+            if Guessing_Number < Random_Number:
+                print('YOUR NUMBER IS LESS THAN RANDOM NUMBER')
+
+                Round()
+            elif Guessing_Number > Random_Number:
+                print('YOUR NUMBER IS GRATER THAN RANDOM NUMBER')
+
+                Round()
+
+
     Round()
+
 Guessing_Number_Game()
