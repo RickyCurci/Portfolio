013d59866130475ac1ca9ac379938bc2e95379fa
RickyCurci <ric.cur@yahoo.com>
2020-09-02 09:25:57 +0200

    Hangman.py

---
+++ python/project/ideas/Hangman.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+words = [ 'riccardo', 'aereodinamic', 'train', 'love', 'ferrari']
+support = ['------', '   |   ', '   |   ', '   |   ', '   |   ', '+---+', '      |']
+
+def printer():
+    for i in range(0,len(support)):
+        print(support[i - 1])
+
+printer()
