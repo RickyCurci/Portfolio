3956f26a7a7af3d15445e504bc2046e40d6f455f
RickyCurci <53341141+RickyCurci@users.noreply.github.com>
2020-09-03 11:48:22 +0200

    Update Hangman.py

--- python/project/ideas/Hangman.py
+++ python/project/ideas/Hangman.py
@@ -1,10 +1,25 @@
-#!/usr/bin/python
-
+#!:/usr/bin/python
+from random import *
 words = [ 'riccardo', 'aereodinamic', 'train', 'love', 'ferrari']
-support = ['+--+', '|  |   ', '   |   ', '   |   ', '======']
+support = ['+--+', '|  |   ', '   |   ', '   |   ', '   |   ', '======']
+man = [ 'O  |   ', '  /|   ', '  /|\\ ']
 
 def printer():
+    
+    word =  words[randint(-1, len(words))].split()
+    X = input("Guessing Word: ")
+    
+    for i in range(0,(len(man) + 2): 
+        for letter in word: 
+            if letter == X: 
+                print('OK')
+            
+            elif letter != X:  
+                support[i] = man[i]
+    
     for i in range(0,len(support)):
         print(support[i])
-
+        
+        
 printer()
+   
