6a9d89024ad1b2ae051b47404c67f186bef5ced5
RickyCurci <ric.cur@yahoo.com>
2020-09-04 00:58:03 +0200

    Update Hangman.py

--- python/project/ideas/Hangman.py
+++ python/project/ideas/Hangman.py
@@ -1,25 +1,50 @@
-#!:/usr/bin/python
+#!/usr/bin/python3
 from random import *
 words = [ 'riccardo', 'aereodinamic', 'train', 'love', 'ferrari']
-support = ['+--+', '|  |   ', '   |   ', '   |   ', '   |   ', '======']
+guessing_letter = []
+
+base = ['+--+', '|  |   ', '   |   ', '   |   ', '   |   ', '======']
 man = [ 'O  |   ', '  /|   ', '  /|\\ ']
 
-def printer():
-    
-    word =  words[randint(-1, len(words))].split()
+words = words[randint(0, len(words))]
+word = [words[int(i)] for i in range(0, len(words))]
+print(word)
+
+
+for i in range(0, len(word)):
+    guessing_letter.append('_')
+
+
+def guessing():
+
     X = input("Guessing Word: ")
-    
-    for i in range(0,(len(man) + 2): 
-        for letter in word: 
-            if letter == X: 
-                print('OK')
-            
-            elif letter != X:  
-                support[i] = man[i]
-    
-    for i in range(0,len(support)):
-        print(support[i])
-        
-        
-printer()
-   
+
+    for letter in word:
+
+        if letter == X:
+            for i in range(-1, 3):
+                position = word.index(letter)
+                guessing_letter[position] = letter
+                word.remove(letter)
+                word[position] = '*'
+                print(word[position])
+                if letter not in word:
+                    break
+
+            printer()
+            print(' ')
+            print(guessing_letter)
+            guessing()
+
+            if guessing_letter == word:
+                print(' ')
+                print('============================')
+                print(' ')
+                print('you win the match')
+
+def printer():
+    for i in range(0,len(base)):
+        print(base[i])
+
+
+guessing()
